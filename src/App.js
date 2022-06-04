d185d67077a21d0d63f7024e0689618e370723da
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-04 17:14:48 +0100

    added NavBar component to page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 // Page components
-import Home from './pages/home/Home'
-import Create from './pages/create/Create'
-import Search from './pages/search/Search'
-import Recipe from './pages/recipe/Recipe'
+import Home from './pages/home/Home.js'
+import Create from './pages/create/Create.js'
+import Search from './pages/search/Search.js'
+import Recipe from './pages/recipe/Recipe.js'
+import NavBar from './components/NavBar.js';
 
 //  Styles
 import './App.css'
@@ -13,6 +14,9 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Route>
+          <NavBar/>
+        </Route>
         <Switch>
         {/* only displays this route */}
           <Route exact path="/">
