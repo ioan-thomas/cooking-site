4df4fb0b04f0c9ba1e85f9cb03d3959ab3015385
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-04 15:55:10 +0100

    added routes for pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// Page components
+import Home from './pages/home/Home'
+import Create from './pages/create/Create'
+import Search from './pages/search/Search'
+import Recipe from './pages/recipe/Recipe'
+
+//  Styles
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+        {/* only displays this route */}
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/recipe/:id">
+            <Recipe />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
