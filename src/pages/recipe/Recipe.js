20327cac4a8f1f85d054750cbec9ef84271393d2
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-05 11:53:22 +0100

    component now fetches data based on route param

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,10 +1,37 @@
 // styles
 import './Recipe.css'
 
-import React from 'react'
+import React, {useEffect} from 'react'
+import { useFetch } from '../../hooks/useFetch'
+import { useParams, useHistory } from 'react-router-dom'
 
 export default function Recipe() {
+  const {id} = useParams()
+  const url = 'http://localhost:3000/recipes/' + id
+  const {data: recipe, isPending, error} = useFetch(url)
+  const history = useHistory()
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        history.push('/')
+      }, 2000)
+    }
+  }, [error, history])
+
   return (
-    <div>Recipe</div>
+    <div className='recipe'>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {recipe && (
+        <>
+          <h1>{recipe.title}</h1>
+          <p>{recipe.ingredients}</p>
+          <h3>{recipe.method}</h3>
+          <p>{recipe.cookingTime}</p>
+          
+        </>
+      )}
+    </div>
   )
 }
