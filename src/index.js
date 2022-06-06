af2e45d277d9e3958018a75c1c7cf98752b76f8f
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-06 15:37:53 +0100

    ThemeProvider.Provider component now encloses App component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+// components
 import App from './App';
+import { ThemeProvider } from './context/ThemeContext'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider.ThemeProvider>
+      <App />
+    </ThemeProvider.ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
