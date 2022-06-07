149a7cc9bcaf23b5d610aca952084663c2f06617
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-07 12:23:54 +0100

    receiving context via ThemeContext.Provider

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -4,8 +4,6 @@ import { ThemeContext } from '../context/ThemeContext'
 
 export const useTheme = () => {
 
-//   const {color} = useContext(ThemeContext) - could have done this way too
-
     const context = useContext(ThemeContext);
     if(context === undefined){
         throw new Error("useTheme() must be used inside a ThemeProvider")
