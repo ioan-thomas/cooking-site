6f2fa86d4c681b2f8c6cec1d4f689357e2f288c7
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-07 20:48:37 +0100

    now receiving info from firebase instead of local server

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,16 +1,45 @@
-// styles
-import './Home.css'
- 
+import { projectFirestore } from '../../firebase/config'
+
 // hooks
-import { useFetch } from '../../hooks/useFetch'
+import { useEffect, useState } from 'react'
+
 
 // components
 import RecipeList from '../../components/RecipeList'
+// styles
+import './Home.css'
 
 
 export default function Home() {
 
-  const {data, isPending, error } = useFetch('http://localhost:3000/recipes/')
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(null);
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    setIsPending(true);
+
+    projectFirestore.collection('recipes')
+      .get()
+      .then(snapshot => {
+        if (snapshot.empty){
+          setError('No recipes to load');
+          setIsPending(false)
+        }else {
+          let results = [];
+          snapshot.docs.forEach(doc => {
+            results.push({id: doc.id, ...doc.data()})
+          })
+          setData(results)
+          setIsPending(false)
+        }
+      }).catch(err => {
+        setError(err.message)
+        setIsPending(false)
+      })
+
+  }, [])
+
   return (
     <div className='home'>
       {error && <p className='error'>{error}</p>}
