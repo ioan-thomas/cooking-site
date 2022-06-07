b7936989d399ddbb66ec949ccf183cc5d0cd685d
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-07 14:18:40 +0100

    each recipe gets dark class based on mode

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,4 +1,4 @@
-
+import {useTheme} from '../hooks/useTheme'
 
 import { Link } from 'react-router-dom'
 
@@ -6,6 +6,8 @@ import { Link } from 'react-router-dom'
 import './RecipeList.css'
 
 export default function RecipeList({recipes}) {
+
+  const {mode} = useTheme()
 
   if (recipes.length === 0){
     return <div className='error'>Sorry, we could not find any recipes with that keyword...</div>
@@ -15,7 +17,7 @@ export default function RecipeList({recipes}) {
   return (
     <div className='recipe-list'>
         {recipes.map(recipe => (
-            <div key={recipe.id} className="card">
+            <div key={recipe.id} className={`card ${mode}`}>
                 <h3>{recipe.title}</h3>
                 <p>{recipe.cookingTime} to make.</p>
                 <div>{recipe.method.substring(0, 100)}...</div>
