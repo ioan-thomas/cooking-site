38ed0204e01c3b2c9df77ca478a17ff0ebe31d88
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-09 20:13:25 +0100

    added realtime document data to update button

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -18,7 +18,7 @@ export default function Recipe() {
 
   useEffect(()=> {
     setIsPending(true);
-    projectFirestore.collection('recipes').doc(id).get().then(doc => {
+    const unsub = projectFirestore.collection('recipes').doc(id).onSnapshot(doc => {
       setIsPending(false)
       if (doc.exists){
         setRecipe(doc.data())
@@ -26,6 +26,8 @@ export default function Recipe() {
         setError('Could not find that recipe')
       }
     })
+
+    return () => unsub()
   }, [id])
 
   const handleClick = () => {
