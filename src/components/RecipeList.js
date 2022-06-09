3aa544cd28f9af088cd254081023b7a3f011d334
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-09 18:01:07 +0100

    added delete functionality when bin is clicked

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,9 +1,12 @@
 import {useTheme} from '../hooks/useTheme'
-
 import { Link } from 'react-router-dom'
+import { projectFirestore } from '../firebase/config'
 
 // styles
 import './RecipeList.css'
+
+// assets
+import TrashCan from '../assets/trash-can.svg'
 
 export default function RecipeList({recipes}) {
 
@@ -13,6 +16,9 @@ export default function RecipeList({recipes}) {
     return <div className='error'>Sorry, we could not find any recipes with that keyword...</div>
   }
 
+  const handleClick = (id) => {
+    projectFirestore.collection('recipes').doc(id).delete()
+  }
 
   return (
     <div className='recipe-list'>
@@ -22,6 +28,13 @@ export default function RecipeList({recipes}) {
                 <p>{recipe.cookingTime} to make.</p>
                 <div>{recipe.method.substring(0, 100)}...</div>
                 <Link to={`/recipes/${recipe.id}`}>Cook This</Link>
+                <img 
+                alt='Trash icon to delete info'
+                src={TrashCan}
+                className='delete mode-toggle'
+                onClick={() => handleClick(recipe.id)}
+                style={{filter: mode === 'dark' ? 'invert(100%)' : 'invert(20%)'}}
+                />
             </div>
         ))}
     </div>
